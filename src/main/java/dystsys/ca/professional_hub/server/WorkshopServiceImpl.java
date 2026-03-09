d9d239dfedd4ede229ed8a9268418c9a05364ccd
ManassesM <mouramana6@gmail.com>
2026-03-09 18:16:33 +0000

    Implement MockDB class, validation and gRPC status error handling

--- src/main/java/dystsys/ca/professional_hub/server/WorkshopServiceImpl.java
+++ src/main/java/dystsys/ca/professional_hub/server/WorkshopServiceImpl.java
@@ -9,6 +9,7 @@ import com.generated.workshop.grpc.GetWorkerNotesRes;
 import com.generated.workshop.grpc.WorkshopGrpc.WorkshopImplBase;
 
 import dystsys.ca.professional_hub.db.MockDB;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class WorkshopServiceImpl extends WorkshopImplBase {
@@ -16,10 +17,22 @@ public class WorkshopServiceImpl extends WorkshopImplBase {
 	// unary call
 	@Override
 	public void checkInWorker(CheckInWorkerReq request, StreamObserver<CheckInWorkerRes> responseObserver) {
-		String workedId = request.getWorkerId();
+		String workerId = request.getWorkerId();
 		String workshopId = request.getWorkshopId();
 		
-		System.out.printf("Checking in worker: %s for workshop %s%n", workedId, workshopId);
+		// check for worker
+		if(!MockDB.REGISTERED_WORKERS.contains(workerId)) {
+			responseObserver.onError(Status.NOT_FOUND.withDescription("Worker ID " + workerId + " was not found on database.").asRuntimeException());
+			return;
+		}
+		
+		// check for workshop id
+		if(!MockDB.WORKSHOP_NOTES.containsKey(workshopId)) {
+			responseObserver.onError(Status.NOT_FOUND.withDescription("Workshop ID " + workshopId + " does not exist.").asRuntimeException());
+			return;
+		}
+		
+		System.out.printf("Checking in worker: %s for workshop %s%n", workerId, workshopId);
 		CheckInWorkerRes response = CheckInWorkerRes.newBuilder().setModuleName("").setWorkshopCredits(10).build();
 	
 		responseObserver.onNext(response);
@@ -32,8 +45,15 @@ public class WorkshopServiceImpl extends WorkshopImplBase {
 		String workshopId = request.getWorkshopId();
 		
 		System.out.printf("Sending material for workshop %s%n", workshopId);
-				
-		List<String> notes = MockDB.WORKSHOP_NOTES.getOrDefault(workshopId, List.of("Invalid workshop ID"));
+		
+		// check for workshop id 
+		if(!MockDB.WORKSHOP_NOTES.containsKey(workshopId)) {
+			responseObserver.onError(Status.NOT_FOUND.withDescription("Workshop ID " + workshopId + " does not exist.").asRuntimeException());
+			return;
+		}
+		
+		// workshop_id exists on db, loop through notes 
+		List<String> notes = MockDB.WORKSHOP_NOTES.get(workshopId);
 		for (String note : notes) {
 			GetWorkerNotesRes response = GetWorkerNotesRes.newBuilder().setNoteContent(note).build();
 			responseObserver.onNext(response);
