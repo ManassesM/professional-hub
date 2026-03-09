90b920816d86a3b24e96d6c6d5113676ada5c0c8
ManassesM <mouramana6@gmail.com>
2026-03-09 13:36:57 +0000

    Initial project structure and proto files

---
+++ src/main/java/dystsys/ca/professional_hub/App.java
@@ -0,0 +1,13 @@
+package dystsys.ca.professional_hub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
